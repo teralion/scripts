9bdd7f8a82b32bb860991f6a7deff50f0b1e0968
user <slavad878@gmail.com>
2022-05-21 15:19:49 +0300

    traverse dir with file stats processing

--- traverse_dir.pl
+++ traverse_dir.pl
@@ -4,34 +4,58 @@ use strict;
 use warnings;
 
 use File::Find;
+use File::stat;
+use Time::localtime;
 
 my @files_changed;
 
 find(\&wanted, ($ARGV[0]));
 sub wanted {
   my $file = $File::Find::name;
+  my $st = stat($file);
 
-  if ( -T $file and /\.[tj]s(x|)$/g ) {
-    my $content = read_file($file);
+  if ( -T $file ) {
+    print "$file\n";
+    print ctime($st->ctime);
+    print "\n";
 
-    if ($content =~ s/console\.log\((\"|\'|\`)( |)#filename/console.log($1$2$_)/g) {
-      push @files_changed, $file;
+    if ( /\.[tj]s(x|)$/g ) {
+      my $content = read_file($file);
+
+      if ($content =~ s/console\.log\((\"|\'|\`)( |)#filename/console.log($1$2$_)/g) {
+        write_file($file, $content);
+        push @files_changed, $file;
+      }
     }
   }
+}
+
+sub write_file {
+  local $/;
+  my $filename = shift;
+  my $content = shift;
+
+  open my $fh, '>', $filename or die "can't open $filename for writing: $!";
+  print $fh $content;
 }
 
 sub read_file {
   local $/;
   my $filename = shift;
 
-  open my $fh, '<:encoding(UTF-8)', $filename or die "can't open $filename: $!";
+  open my $fh, '<:encoding(UTF-8)', $filename or die "can't open $filename for reading: $!";
   return <$fh>;
 }
 
 {
   local $, = "\n";
-  print @files_changed;
-  print "\n";
+  if (@files_changed) {
+    print "files changed:\n";
+    print @files_changed;
+    print "\n";
+  } else {
+    print "no files changed\n";
+  }
 }
 
 1;
